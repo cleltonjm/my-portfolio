88ab90d05030d118144e983559dd22e9fe9dae45
Clelton <cleltonjm_95@hotmail.com>
2025-05-16 14:48:12 -0300

    feat: create services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./components/home/home.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { ProjectsComponent } from "./components/projects/projects.component";
+import { ServicesComponent } from "./components/services/services.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, HomeComponent, FooterComponent, ProjectsComponent],
+  imports: [HeaderComponent, HomeComponent, FooterComponent, ProjectsComponent, ServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
