31e0c97e07c155599b3a6053acd5911304f41da0
Clelton <cleltonjm_95@hotmail.com>
2025-05-16 18:31:40 -0300

    feat: create skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from "./components/home/home.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { ProjectsComponent } from "./components/projects/projects.component";
 import { ServicesComponent } from "./components/services/services.component";
+import { SkillsComponent } from "./components/skills/skills.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, HomeComponent, FooterComponent, ProjectsComponent, ServicesComponent],
+  imports: [HeaderComponent, HomeComponent, FooterComponent, ProjectsComponent, ServicesComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
