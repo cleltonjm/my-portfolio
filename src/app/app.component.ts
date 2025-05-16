120a1abb9fda281a6db59d35b67c7400733cd2af
Clelton <cleltonjm_95@hotmail.com>
2025-05-16 12:33:28 -0300

    feat: projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./components/home/home.component";
 import { FooterComponent } from './components/footer/footer.component';
+import { ProjectsComponent } from "./components/projects/projects.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, HomeComponent, FooterComponent],
+  imports: [HeaderComponent, HomeComponent, FooterComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
